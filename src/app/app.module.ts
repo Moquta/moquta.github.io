15414d0d684d22754afd0ee4b515588e52495e3f
Anwar A. Musa <anwaramusa@gmail.com>
2022-11-21 15:34:22 -0500

    finished settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
@@ -24,6 +25,7 @@ import { SettingsComponent } from './pages/settings/settings.component';
     FormsModule,
     MatButtonModule,
     MatDividerModule,
+    MatIconModule,
     MatInputModule,
     MatSelectModule,
   ],
--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -11,11 +11,15 @@ import { IMoqutaSettings, SettingsService } from 'src/app/services';
 export class SettingsComponent implements OnInit {
   settings: IMoqutaSettings;
   prayerTimesTune: string[];
+  additionalInfo: { value: string }[];
 
   @ViewChild('form', { static: true }) ngForm!: NgForm;
   constructor(private settingsService: SettingsService) {
     this.settings = this.settingsService.getSettings();
     this.prayerTimesTune = this.settings.ApiParams.tune?.split(',') ?? [];
+    this.additionalInfo = this.settings.AdditionalInfo.map((value) => ({
+      value,
+    }));
   }
 
   ngOnInit(): void {
@@ -27,6 +31,9 @@ export class SettingsComponent implements OnInit {
   saveSettings(): void {
     if (this.settings) {
       this.settings.ApiParams.tune = this.prayerTimesTune.join(',');
+      this.settings.AdditionalInfo = this.additionalInfo.map(
+        (value) => value.value
+      );
       this.settingsService.saveSettings(this.settings);
     }
   }
