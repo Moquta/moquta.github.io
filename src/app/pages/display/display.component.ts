e515ffcbd6e06b019b5529201f438b20cabc56f4
Elyasmusa <62361971+Elyasmusa@users.noreply.github.com>
2022-12-25 22:58:51 -0500

    Mqta 15: Countdown for iqamah
    * Iqama Countdown
    
    Started work on countdown 2min before iqama
    
    * Added Circle
    
    Added Circle bar for 2min timer
    
    * Made changes
    
    made requested changes

--- src/app/pages/display/display.component.ts
+++ src/app/pages/display/display.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
 import * as dayjs from 'dayjs';
 
 import {
   AdhanService,
   IAdhanApiCityParams,
+  IDailyPrayerTimes,
   IMoqutaSettings,
   IPrayerTimesDayData,
   IPrayerTimesYearData,
@@ -18,10 +21,22 @@ import {
 })
 export class DisplayComponent {
   now = new Date();
+
   today = new Date();
 
   settings: IMoqutaSettings;
   prayerData!: IPrayerTimesDayData;
+
+  
+  get progressbarValue(): number{
+    if (this.showCountdown){
+      return (100 - ((this.nextPrayerIqama.valueOf() - this.now.valueOf())/120000)*100);
+      
+    }
+    else
+      return 100; 
+  }
+
 
   // TODO? Is there a better, more efficient way to do this?
   get nextPrayerIndex(): number {
@@ -83,6 +98,88 @@ export class DisplayComponent {
 
     return pIndex;
   }
+  get nextPrayerTime(): string {
+    let nextPrayerTime = this.prayerData.timings.Fajr;
+    switch (this.nextPrayerIndex) {
+      case 0:
+        nextPrayerTime = this.prayerData.timings.Fajr;
+        break;
+      case 1:
+        nextPrayerTime = this.prayerData.timings.Sunrise;
+        break;
+      case 2:
+        nextPrayerTime = this.prayerData.timings.Dhuhr;
+        break;
+      case 3:
+        nextPrayerTime = this.prayerData.timings.Asr;
+        break;
+      case 4:
+        nextPrayerTime = this.prayerData.timings.Maghrib;
+        break;
+      case 5:
+        nextPrayerTime = this.prayerData.timings.Isha;
+        break;
+    }
+    return nextPrayerTime;
+  }
+
+  get nextPrayerIqama(): Date {
+    let nextPrayerIqama = this.calcIqamaTime(
+      this.prayerData.timings.Fajr,
+      this.settings.IqamaTimings.Fajr
+    );
+    const format = 'HH:mm';
+    switch (this.nextPrayerIndex) {
+      case 0:
+        nextPrayerIqama = this.calcIqamaTime(
+          this.prayerData.timings.Fajr,
+          this.settings.IqamaTimings.Fajr,
+          format
+        );
+        break;
+      case 1:
+        nextPrayerIqama = this.prayerData.timings.Sunrise;
+        break;
+      case 2:
+        nextPrayerIqama = this.calcIqamaTime(
+          this.prayerData.timings.Dhuhr,
+          this.settings.IqamaTimings.Dhuhr,
+          format
+        );
+        break;
+      case 3:
+        nextPrayerIqama = this.calcIqamaTime(
+          this.prayerData.timings.Asr,
+          this.settings.IqamaTimings.Asr,
+          format
+        );
+        break;
+      case 4:
+        nextPrayerIqama = this.calcIqamaTime(
+          this.prayerData.timings.Maghrib,
+          this.settings.IqamaTimings.Maghrib,
+          format
+        );
+        break;
+      case 5:
+        nextPrayerIqama = this.calcIqamaTime(
+          this.prayerData.timings.Isha,
+          this.settings.IqamaTimings.Isha,
+          format
+        );
+        break;
+    }
+    return new Date(dayjs(this.today).format('YYYY-MM-DD').toString() + ' ' + nextPrayerIqama)
+  }
+
+  get showCountdown(): boolean {
+ 
+    return (
+      dayjs(this.now).isBefore(dayjs(this.nextPrayerIqama)) &&
+      dayjs(this.now).isAfter(dayjs(this.nextPrayerIqama).subtract(2, 'minutes'))
+    );
+    //dayjs(nextIqamaDate).toDate().valueOf() - this.now.valueOf()
+  }
 
   constructor(
     private adhanApi: AdhanService,
@@ -92,7 +189,7 @@ export class DisplayComponent {
     this.retrievePrayerTimes();
 
     setInterval(() => {
-      // this.now = dayjs(this.now).add(1, 'hour').toDate();
+      //this.now = dayjs(this.now).add(5, 'second').toDate();
       this.now = new Date();
       if (this.today.getDate() !== this.now.getDate()) {
         this.today = this.now;
